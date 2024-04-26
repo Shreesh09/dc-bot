012ba7d18c19f6a3a7c53180f21dc007dff83337
Shreesh <shreeshnautiyal3@gmail.com>
2024-04-26 14:45:56 +0530

    fix: add http server for to prevent server shutdown

--- bot.js
+++ bot.js
@@ -33,3 +33,19 @@ client.once('ready', () => {
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN); 
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+ if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Hello JSON' }));
+ } else {
+    res.writeHead(404);
+    res.end();
+ }
+});
+
+server.listen(process.env.PORT, () => {
+ console.log('Server running');
+});
